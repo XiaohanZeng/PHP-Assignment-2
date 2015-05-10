595eeaf8ff4bc222aa27a513a1d5804c23db2576
unknown <zengx@onid.oregonstate.edu>
2015-05-10 11:51:10 -0700

    test commit

--- src/videoTrack.php
+++ src/videoTrack.php
@@ -79,7 +79,7 @@ function addNew()
 	$name = $_GET['Name'];
 	$category = $_GET['Category'];
 	$length = $_GET['Length'];
-	$all = $mysqli->prepare("INSERT INTO $table (Name, Category, Length) VALUES('$name','$category',$length)");
+	$all = $mysqli->prepare("INSERT INTO $table (Name, Category, Length) VALUES('$name','$category','$length')");
 	$all->execute();
 	init();
 }
